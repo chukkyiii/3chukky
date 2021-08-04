544cf35cbec59fb0faee35fedcf380bd662cf937
chukkyiii <jgodwin1420@gmail.com>
2021-08-04 17:28:48 +0100

    updated changes with links

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,14 +1,18 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import Layout from '../../components/layout'
 
 const BlogPage = ({ data }) => {
   return (
-    <Layout pageTitle="My Blog Posts">
+    <Layout pageTitle="Blog">
       {
         data.allMdx.nodes.map(node => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
           </article>
         ))
@@ -26,6 +30,7 @@ export const query = graphql`
           title
         }
         id
+        slug
       }
     }
   }

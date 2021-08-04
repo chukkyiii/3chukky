f0f73b2bba2bb5f2a445ee3efc9558ec4dfe5037
chukkyiii <jgodwin1420@gmail.com>
2021-08-04 16:59:52 +0100

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,22 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "3chukky",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: ["gatsby-plugin-gatsby-cloud",
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/ // See below to configure properly https://www.gatsbyjs.com/plugins/gatsby-plugin-react-svg/
+        }
+      }
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    `gatsby-plugin-mdx`
+  ],
 };
---
+++ src/components/layout.js
@@ -0,0 +1,60 @@
+import * as React from 'react'
+import Icon from "../assets/icon.svg"
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import {
+    nav_links,
+    logo,
+    heading,
+    infoBox
+} from './layout.module.css'
+
+const Layout = ({ children, pageTitle }) => {
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
+        }
+    `)
+
+    return (
+        <main>
+            <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+            <header>
+                <Icon className={logo} />
+                <nav>
+                    <ul className={nav_links}>
+                        <li>
+                            <Link to='/'>
+                                README.
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/'>
+                                PROJECTS.
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/'>
+                                BLOG.
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Link to='/'>
+                    <button>Contact</button>
+                </Link>
+            </header>
+            <div className={infoBox}>
+                <h1 className={heading}>{pageTitle}</h1>
+            </div>
+            <div className={infoBox}>
+                {children}
+            </div>
+        </main>
+    )
+}
+
+export default Layout

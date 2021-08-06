78c0126940fe588dd8197a82bb3013074a6f3d15
chuks <jodwin1420@gmail.com>
2021-08-06 15:58:28 +0100

    Added more to the paragraph

--- src/components/content.js
+++ src/components/content.js
@@ -17,20 +17,17 @@ const Content = ({ children }) => {
             <article className={articleStyle}>
                 <h1 className={headingOne}>
                     <Icon className={logo} />
-                    HI. <br /> MY NAME IS CHUKKY
+                    Hi. <br /> My name is Chukky.
                 </h1>
                 <h2 className={headingTwo}>I code, write, and contribute to open source software.</h2>
                 <p className={paragraphStyle}> 
                     I’m currently brain-storming + building my website! I’m also currently learning gatsby.js and using it for this site.
                     I’m looking to collaborate on any projects that involve web dev. Currently working on <a href="https://github.com/chukkyiii/ONO"> <code>ONO</code></a>
-                    and learning <code>React.</code> You can reach me via email: <code>jgodwin1420@gmail.com</code> or <code>{discName}#5111</code> 
-                    on discord.
+                    using the <code>Flask</code> webframework and learning <code>React.</code> You can reach me on discord with <code>{discName}#5111</code>.
                 </p>
                 <a href="/blog" >
                     <button>Blog</button>
-                </a>
-                
-                
+                </a> 
             </article>
         </main>
     )

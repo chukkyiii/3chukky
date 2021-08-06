953e1e896eca7aa932f8eaf06b7b1969b652b2ec
chuks <jodwin1420@gmail.com>
2021-08-06 16:14:50 +0100

    added color to the addition sign

--- src/components/content.js
+++ src/components/content.js
@@ -3,6 +3,7 @@ import Icon from '../assets/icon.svg'
 
 import {
     all,
+    add,
     headingOne,
     headingTwo,
     paragraphStyle,
@@ -21,7 +22,7 @@ const Content = ({ children }) => {
                 </h1>
                 <h2 className={headingTwo}>I code, write, and contribute to open source software.</h2>
                 <p className={paragraphStyle}> 
-                    I’m currently brain-storming + building my website! I’m also currently learning gatsby.js and using it for this site.
+                    I’m currently brain-storming <b className={add}>+</b> building my website! I’m also currently learning gatsby.js and using it for this site.
                     I’m looking to collaborate on any projects that involve web dev. Currently working on <a href="https://github.com/chukkyiii/ONO"> <code>ONO</code></a>
                     using the <code>Flask</code> webframework and learning <code>React.</code> You can reach me on discord with <code>{discName}#5111</code>.
                 </p>

00b2ac79528cf3f9f0fec97b8db609263d062e30
chuks <jgodwin1420@gmail.com>
2021-09-02 21:05:54 +0100

    Add to hgihight

--- src/components/content.js
+++ src/components/content.js
@@ -3,7 +3,7 @@ import Icon from '../assets/icon.svg'
 
 import {
     all,
-    add,
+    highlight,
     headingOne,
     headingTwo,
     paragraphStyle,
@@ -23,7 +23,7 @@ const Content = ({ Title, children }) => {
                 </h1>
                 <h2 className={headingTwo}>I code, write, and contribute to open source software.</h2>
                 <p className={paragraphStyle}> 
-                    I’m currently brain-storming <b className={add}>+</b> building my website! I’m also currently learning gatsby.js and using it for this site.
+                    I’m currently brain-storming <span className={highlight}>+</span> building my website! I’m also currently learning gatsby.js and using it for this site.
                     I’m looking to collaborate on any projects that involve web dev. Currently working on <a href="https://github.com/chukkyiii/ONO"> <code>ONO</code></a>
                     using the <code>Flask</code> webframework and learning <code>React.</code> You can reach me on discord with <code>{discName}#5111</code>.
                 </p>

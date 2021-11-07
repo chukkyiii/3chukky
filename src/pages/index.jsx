76898d8ad67584e98c684ca69f40cd2f83b24ed9
chuks <jgodwin1420@gmail.com>
2021-11-07 22:22:39 +0000

    .

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import '../styles/global.css'
 import Content from '../components/content'
 
+
 const IndexPage = () => {
     let word = Math.random().toString(36).substring(2,7);
     let yourTitle = word.concat(' Is obviously a word')

63c91fa5b053012c90ea8c882b657a481bd649ee
chukkyiii <jgodwin1420@gmail.com>
2021-11-08 23:43:09 +0000

    updated status

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -24,8 +24,11 @@ const Content = ({ Title, children }) => {
                 </h1>
                 <h2 className={headingTwo}>I code, write, and contribute to open source software.</h2>
                 <p className={paragraphStyle}> 
-                    <span className={highlight}>I’m currently brain-storming!</span> I’m looking to collaborate on any projects. Currently working on <a href="https://github.com/chukkyiii/ONO">ONO</a>
-                    using the <a href='https://flask.palletsprojects.com/en/2.0.x/'>Flask</a>webframework and learning <a href='https://reactjs.org/'>React.</a> You can reach me on discord with <code>{discName}#5111</code>.
+                    I’m currently working on <a href="https://reacton.tech">reacton.tech</a><span className={highlight}>+</span> I’m looking to collaborate on any projects. 
+                    I have other projects like <a href="https://github.com/chukkyiii/ONO">ONO</a>using <a href='https://flask.palletsprojects.com/en/2.0.x/'>Flask</a> and   
+                    <a href="https://github.com/chukkyiii/Wault"> Wault</a>a Json Table Generator. I am mostly learning Python, Javascript more specifically <a href='https://reactjs.org/'>React</a>
+                    and Java. I am also writing blogs on what I am working on, tips and also solutions to problems I may encounter. <br/>
+                    You can reach me on discord with <code>{discName}#5111</code>.
                 </p>
                 <a href="/blog" >
                     <button className={blogButton}>Blog</button>

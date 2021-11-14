8431129cb6c84c713a0d621a5af421e39165f326
chukkyiii <jgodwin1420@gmail.com>
2021-11-14 01:06:01 +0000

    redesigned readme page

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,30 +1,29 @@
 import * as React from 'react'
-import Icon from '../assets/icon.svg'
+import Name from '../assets/name.svg'
 
 import {
     all,
-    highlight,
+    highlightg,
+    highlighto,
     headingOne,
-    headingTwo,
     paragraphStyle,
     articleStyle,
     blogButton,
     logo,
 } from './content.module.css'
 
-const Content = ({ Title, children }) => {
+const Content = ({Title, children }) => {
     const discName = '!{chukky}'
     return (
         <main className={all}>
             <title>{Title}</title>
             <article className={articleStyle} >
                 <h1 className={headingOne}>
-                    <Icon className={logo} />
-                    Hi. <br /> My name is Chukky.
+                    CREATION BY <Name classname={logo}/> <br />
+                    MOSTLY CONTAINING <span className={highlighto}>CODE</span> & <span className={highlighto}>DESIGN</span>
                 </h1>
-                <h2 className={headingTwo}>I code, write, and contribute to open source software.</h2>
                 <p className={paragraphStyle}> 
-                    I’m currently working on <a href="https://reacton.tech">reacton.tech</a><span className={highlight}>+</span> I’m looking to collaborate on any projects. 
+                    I’m currently working on <a href="https://reacton.tech">reacton.tech</a><span className={highlightg}>+</span> I’m looking to collaborate on any projects. 
                     I have other projects like <a href="https://github.com/chukkyiii/ONO">ONO</a>using <a href='https://flask.palletsprojects.com/en/2.0.x/'>Flask</a> and   
                     <a href="https://github.com/chukkyiii/Wault"> Wault</a>a Json Table Generator. I am mostly learning Python, Javascript more specifically <a href='https://reactjs.org/'>React</a>
                     and Java. I am also writing blogs on what I am working on, tips and also solutions to problems I may encounter. <br/>

c78a4ce3bcf9efc5753991235ec1dc593e937ced
chukkyiii <jgodwin1420@gmail.com>
2021-12-07 16:18:44 +0000

    basic start page

---
+++ src/pages/projects/index.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import '../../styles/global.css'
+import Layout from '../../components/layout'
+
+
+const ProjectPage = () => {
+    return (
+        <Layout pageTitle="Projects">
+        </Layout>
+    )
+}
+
+export default ProjectPage
+

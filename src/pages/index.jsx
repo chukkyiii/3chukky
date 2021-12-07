a0d620552f94347306f0aa8a8b3f5702d0322122
chukkyiii <jgodwin1420@gmail.com>
2021-12-07 16:23:15 +0000

    add layout and removed blog button

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,17 @@
 import * as React from 'react'
 import '../styles/global.css'
 import Content from '../components/content'
+import Layout from '../components/layout'
 
 
 const IndexPage = () => {
     let word = Math.random().toString(36).substring(2,7);
     let yourTitle = word.concat(' Is obviously a word')
     return (
-        <Content Title={yourTitle}>
-        </Content>
+        <Layout pageTitle={yourTitle}>
+            <Content>
+            </Content>
+        </Layout>
     )
 }
 

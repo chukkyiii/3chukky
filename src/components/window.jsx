22b1143b46e0dcf5a03d1a5cddbeb1fefa4eec2e
chuks <jgodwin1420@gmail.com>
2022-01-16 00:50:57 +0000

    Finds screen size useful for responsive styling

---
+++ src/components/window.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import useWindowDimensions from '../hooks/useWindowDimensions';
+
+const Window = () => {
+  const { height, width } = useWindowDimensions();
+
+  return (
+    <div>
+      width: {width} ~ height: {height}
+    </div>
+  );
+}
+
+export default Window
